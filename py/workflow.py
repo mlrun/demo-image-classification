f57886500467b38d704b8d65c6e01fe72b1c8a29
Or Zilberman <zilbermanor@gmail.com>
2020-02-21 15:00:26 +0200

    - function yamls - edited workflow to match function yamls

--- py/workflow.py
+++ py/workflow.py
@@ -1,4 +1,8 @@
+from kfp import dsl
 from mlrun import mount_v3io
+
+artifcats_path = './'
+funcs = {}
 
 
 def init_functions(functions: dict, params=None, secrets=None):
@@ -19,3 +23,51 @@ def init_functions(functions: dict, params=None, secrets=None):
     '''
     for f in functions.values():
         f.apply(mount_v3io())
+
+
+@dsl.pipeline(
+    name='Image classification demo',
+    description='Train an Image Classification TF Algorithm using MLRun'
+)
+def kfpipeline(
+        image_archive='http://iguazio-sample-data.s3.amazonaws.com/catsndogs.zip',
+        images_path='/User/mlrun/examples/images',
+        source_dir='/User/mlrun/examples/images/cats_n_dogs',
+        checkpoints_dir='/User/mlrun/examples/checkpoints',
+        model_path='/User/mlrun/examples/models/cats_n_dogs.h5',
+        model_name='cat_vs_dog_v1'):
+    # First we need to build our function containers
+    builder_utils = funcs['utils'].deploy_step(with_mlrun=True)
+
+    open_archive = funcs['utils'].as_step(name='download',
+                                          handler='open_archive',
+                                          out_path=images_path,
+                                          params={'target_dir': images_path},
+                                          inputs={'archive_url': image_archive},
+                                          outputs=['content']).apply(
+        mount_v3io())
+
+    label = funcs['utils'].as_step(name='label',
+                                   handler='categories_map_builder',
+                                   out_path=images_path,
+                                   params={'source_dir': source_dir},
+                                   outputs=['categories_map',
+                                            'file_categories']).after(
+        open_archive)
+
+    train = funcs['trainer'].as_step(name='train',
+                                     params={'epochs': 3,
+                                             'checkpoints_dir': checkpoints_dir,
+                                             'model_path': model_path,
+                                             'data_path': source_dir},
+                                     inputs={'categories_map': label.outputs[
+                                         'categories_map'],
+                                             'file_categories': label.outputs[
+                                                 'file_categories']},
+                                     outputs=['model'])
+
+    # deploy the model using nuclio functions
+    deploy = funcs['serving'].deploy_step(project='nuclio-serving',
+                                          models={
+                                              model_name: train.outputs[
+                                                  'model']})
